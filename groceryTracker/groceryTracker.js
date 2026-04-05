df4489dd145db01ce1a4ebc3c4bd86812b4cfafe
Gatchu69 <nodariko.gachava@gmail.com>
2026-04-05 23:57:08 +0400

    Sum Grocery Costs

---
+++ groceryTracker/groceryTracker.js
@@ -0,0 +1,9 @@
+function calculateTotal() {
+    grocery1 = parseFloat(document.getElementById("grocery-1").value);
+    grocery2 = parseFloat(document.getElementById("grocery-2").value);
+    grocery3 = parseFloat(document.getElementById("grocery-3").value);
+
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById("total").innerText = `The total amount is: ${total}`;
+}
